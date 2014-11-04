20e92c7f4d5be0f99c239a324dbe5d2e37b1aef1
Rodney Cobb <roacobb@gmail.com>
2014-11-04 17:51:07 +0000

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oce57fea8771',
+  'passwordsalt' => 'c425c45eb5125005be3f075471689e',
+  'trusted_domains' => 
+  array (
+    0 => '162.242.254.82',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'dbtype' => 'mysql',
+  'version' => '7.0.2.1',
+  'dbname' => 'test',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => '61926a7c3c0318187c70689d8e38a7',
+  'installed' => true,
+);
---
+++ core/doc/admin/_sources/maintenance/index.txt
@@ -0,0 +1,13 @@
+===========
+Maintenance
+===========
+
+.. toctree::
+   :maxdepth: 1
+
+   enable_maintenance
+   backup
+   update
+   restore
+   migrating
+
